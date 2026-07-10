63d3453317deb9e57203ca1f55e08a1dc0be3124
itidabhoi-wq <itidabhoi@gmail.com>
2026-07-10 19:38:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function searchResult() {
 
   let regNo = document.getElementById("regNo").value.trim();
 
-  fetch("YOUR_WEB_APP_URL?regNo=" + encodeURIComponent(regNo))
+  fetch("https://script.google.com/macros/s/AKfycbzy7M7ALuaE3h3IHgb_8VEnPJPdAickIn_uPJQ_wpjGdYlEwS7fFEo2KVJoyNlihCltKg/exec?regNo=" + encodeURIComponent(regNo))
     .then(response => response.json())
     .then(data => {
 
