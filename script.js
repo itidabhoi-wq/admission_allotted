0a69e054078f9b7e59f36f9ba620aeeb5966e363
itidabhoi-wq <itidabhoi@gmail.com>
2026-07-10 18:43:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-function searchResult() {
+function searchResult(){
 
     let regNo = document.getElementById("regNo").value.trim();
 
-    if (regNo === "123456") {
+    fetch("YOUR_WEB_APP_URL?regNo=" + encodeURIComponent(regNo))
 
-        document.getElementById("result").innerHTML = `
-            <h3>Result Found</h3>
-            <p><b>Name:</b> Rahul Patel</p>
-            <p><b>Trade:</b> COPA</p>
-            <p><b>ITI:</b> Government ITI Dabhoi</p>
-        `;
+    .then(response => response.json())
 
-    } else {
+    .then(data => {
 
-        document.getElementById("result").innerHTML =
-        "<h3>No Record Found</h3>";
+        if(data.found){
 
-    }
+            document.getElementById("result").innerHTML = `
+                <h3>Allocated Trade</h3>
+                <p>${data.trade}</p>
+            `;
+
+        }else{
+
+            document.getElementById("result").innerHTML =
+            "<h3>No Record Found</h3>";
+
+        }
+
+    });
 
 }
