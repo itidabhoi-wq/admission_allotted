14a4b9501de891feb3c57ef9675dabce093c0cb7
itidabhoi-wq <itidabhoi@gmail.com>
2026-07-10 19:36:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-function searchResult(){
+function searchResult() {
 
-    let regNo = document.getElementById("regNo").value.trim();
+  let regNo = document.getElementById("regNo").value.trim();
 
-    fetch("YOUR_WEB_APP_URL?regNo=" + encodeURIComponent(regNo))
-
+  fetch("YOUR_WEB_APP_URL?regNo=" + encodeURIComponent(regNo))
     .then(response => response.json())
-
     .then(data => {
 
-        if(data.found){
+      if (data.found) {
 
-            document.getElementById("result").innerHTML = `
-                <h3>Allocated Trade</h3>
-                <p>${data.trade}</p>
-            `;
+        document.getElementById("result").innerHTML =
+          "<h2>Allocated Trade</h2>" +
+          "<p><b>Registration No:</b> " + data.registrationNo + "</p>" +
+          "<p><b>Trade:</b> " + data.trade + "</p>";
 
-        }else{
+      } else {
 
-            document.getElementById("result").innerHTML =
-            "<h3>No Record Found</h3>";
+        document.getElementById("result").innerHTML =
+          "<h3>No Record Found</h3>";
 
-        }
+      }
 
     });
 
