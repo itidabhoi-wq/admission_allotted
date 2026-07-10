39dafedfa73261b7c00ab72a187350fa313b398c
itidabhoi-wq <itidabhoi@gmail.com>
2026-07-10 20:45:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function searchResult() {
 
+
+  alert("કૃપા કરીને Search પર ક્લિક કર્યા બાદ થોડો સમય રાહ જુઓ.  ⚠️ નોંધ: સદર પરિણામમાં ફેરફાર થઈ શકે છે. તેથી ફાળવેલ આઈ.ટી.આઈ. ખાતે રૂબરૂ જઈ માહિતીની ખાતરી અવશ્ય કરવી. અંતિમ નિર્ણય DET, ગુજરાત દ્વારા જાહેર કરવામાં આવેલી સત્તાવાર ITI Admission પોર્ટલ પર રહેલ માહિતી મુજબ જ માન્ય ગણાશે");
   let regNo = document.getElementById("regNo").value.trim();
 
   fetch("https://script.google.com/macros/s/AKfycbzy7M7ALuaE3h3IHgb_8VEnPJPdAickIn_uPJQ_wpjGdYlEwS7fFEo2KVJoyNlihCltKg/exec?regNo=" + encodeURIComponent(regNo))    .then(response => response.json())
